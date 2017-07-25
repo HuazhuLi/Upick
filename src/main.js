da309d39906b1ad2ae7e13324162fdf72846f891
Faraway <zhiyuanhong@hustunique.com>
2017-07-26 03:01:21 +0800

    怎么能少得了console彩蛋呢=￣ω￣=

--- src/main.js
+++ src/main.js
@@ -7,6 +7,19 @@
  */
 
 import './plugins/zhugeio'
+
+const consoleLogStyle = `
+line-height: 50px;
+height: 50px;
+display: inline-block;
+background: #fe921f;
+color: #FFFFFF;
+padding: 20px 20px 16px 20px;
+`
+
+console.log(`%c    Upick!   `, consoleLogStyle + 'font-size:32px;')
+console.log(`%cPowered By Bingyan Studio`, consoleLogStyle + 'font-size:16px;padding-right:23px;');
+
 (async function () {
   /**
    * Async-loading of this App, will be
