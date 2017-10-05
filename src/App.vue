b7869913b8f7ff14baf6924559b2f09604e8b3bd
Faraway <zhiyuanhong@hustunique.com>
2017-10-05 19:27:39 +0800

    修改验证登录的条件

--- src/App.vue
+++ src/App.vue
@@ -22,7 +22,7 @@ export default {
     })
     this.initConfig()
     this.makeConfig()
-    if ((window.location.href.indexOf('weixin.bingyan-tech.hustonline.net') >= 0 && window.location.href.indexOf('dev') < 0) ||
+    if ((window.location.href.indexOf('weixin.bingyan-tech.hustonline.net') >= 0 && window.location.href.indexOf('dev') < 0) &&
       (await checkLoginStatus()) === false && process.env.NODE_ENV !== 'development') {
       window.title = '需要登录！'
       window.location.href = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx70014cb42f7c9422&redirect_uri=http%3A//weixin.bingyan-tech.hustonline.net/upick/api/v2/weixin/access&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect'
